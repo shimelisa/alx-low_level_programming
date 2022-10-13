328e7dbe85a85cd02d61e5723cf64ff2142b2b59
shimelisa <shimyk97@gmail.com>
2022-10-13 13:41:01 -0700

    My first c programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * This is main block
+ * The program is to print "Programming is like building a multilingual puzzle."
+ */
+
+int main (void)
+{
+	puts("\"Programming is like bulding a mulitilingual puzzle.\"\n");
+	return (0);
+}
