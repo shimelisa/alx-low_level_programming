6ac2a71536ced24e55dd6598800b1dd25911c458
shimelisa <shimyk97@gmail.com>
2022-10-13 14:03:35 -0700

    program to print some statement

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main: always required
+ * return: return is always 0 for main
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
