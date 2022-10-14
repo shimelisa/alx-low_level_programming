d9bc1b3565fbfa33645e07dab49b3638e771aa42
shimelisa <shimyk97@gmail.com>
2022-10-14 10:55:02 -0700

    return last digit of a number and check if it is >5 or =0 or <6

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - main header
+* Description: this code check if num is +ve or -ve
+* Return: 0 (always)
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int last_digit;
+	last_digit = n % 10;
+
+	printf("Last digit of %d is %d", n, last_digit);
+
+	if (last_digit > 5)
+		printf(" and is greater than 5\n");
+	else if (last_digit == 0)
+		printf(" and is 0\n");
+	else
+		printf("and is less than 6 and no 0\n");
+
+	return (0);
+}
