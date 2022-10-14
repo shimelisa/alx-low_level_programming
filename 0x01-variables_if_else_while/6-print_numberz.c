f9b155c5411f3ffd6dea92be2b0f73ffe4b632e9
shimelisa <shimyk97@gmail.com>
2022-10-14 15:08:17 -0700

    print num b/n 0 & 9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main - main funciton
-* Description: This program display number 0 to 9
-* Return: 0 (always 0 for main
+* main - main body
+* Description: this program dispay numbers
+* Retirm: 0 (always 0 for main)
 */
 int main(void)
 {
 	int num;
 
 	for (num = 0; num < 10; num++)
-		printf(%i, num);
+		putchar(num);
 	putchar('\n');
 
 	return (0);
