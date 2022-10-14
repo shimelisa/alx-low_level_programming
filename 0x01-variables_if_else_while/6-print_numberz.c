15b78dd5d3f608b68a6d909a5222be2f040148a8
shimelisa <shimyk97@gmail.com>
2022-10-14 15:12:06 -0700

    print num b/n 0 & 9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,14 +3,14 @@
 /**
 * main - main body
 * Description: this program dispay numbers
-* Retirm: 0 (always 0 for main)
+* Return: 0 (always 0 for main)
 */
 int main(void)
 {
 	int num;
 
 	for (num = 0; num < 10; num++)
-		putchar(num);
+		putchar(num + '0');
 	putchar('\n');
 
 	return (0);
