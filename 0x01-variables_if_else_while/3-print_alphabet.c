43d5f324acd9f24b5c762b4cc992eee103dbe049
shimelisa <shimyk97@gmail.com>
2022-10-14 13:49:47 -0700

    print smal and capital letters

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - print lower and upercase letter
+* Description: this program prints lower & uppercase letters
+* Return: 0 (always 0 for main)
+*/
+
+int main(void)
+{
+	char sml_cap;
+
+	for (sml_cap ='a'; sml_cap <= 'z'; sml_cap++)
+		putchar(sml_cap);
+	for (sml_cap = 'A'; sml_cap <= 'Z'; sml_cap++)
+		putchar(sml_cap);
+	putchar('\n');
+
+	return (0);
+}
