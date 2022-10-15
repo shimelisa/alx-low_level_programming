ecfa8d8e95ba0da9965111ca58a83fbbbc5182ba
shimelisa <shimyk97@gmail.com>
2022-10-14 17:05:21 -0700

    print combination of 3-digit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - main body of program
+* Description: this program runs combination of 3-digits
+* Return: 0 (always 0 for main body)
+*/
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+
+				if (i < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	return (0);
+}
