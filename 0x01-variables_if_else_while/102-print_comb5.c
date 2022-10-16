a2c28be06ae8ce9d1d2c1bb7e2633ea77dc761b1
shimelisa <shimyk97@gmail.com>
2022-10-16 03:54:06 -0700

    print possible combinatin of two two-digt#

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,30 +8,67 @@
 int main(void)
 {
 	int i, j, k;
+	char str[2];
 
-	for (i = 0; i <= 29; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j = i + 1; j <= 29; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 			if (i < 10)
 			{
 				putchar(0 + '0');
+				putchar(i + '0');
+				putchar(' ');
 			}
-			putchar(i + '0');
-			putchar(' ');
+			else
+			{
+				str[0] = i / 10;
+				str[1] = i % 10;
+
+				putchar(str[0] + '0');
+				putchar(str[1] + '0');
+				putchar(' ');
+			}
 
 			if (j < 10)
 			{
 				putchar(0 + '0');
-			}
-			putchar(j + '0');
-
-			if (j < 29)
-			{
+				putchar(j + '0');
 				putchar(',');
+				putchar(' ');
 			}
+			else if (j < 99)
+			{
+				str[0] = j / 10;
+				str[1] = j % 10;
+
+				putchar(str[0] + '0');
+				putchar(str[1] + '0');
+				putchar(',');
+				putchar(' ');
+			}
+			else if (j == 99 & i < 98)
+			{
+				str[0] = j / 10;
+				str[1] = j % 10;
+
+				putchar(str[0] + '0');
+				putchar(str[1] + '0');
+				putchar(',');
+				putchar(' ');
+			}
+			else if (j == 99 && i == 98)
+			{
+				str[0] = j / 10;
+				str[1] = j % 10;
+
+				putchar(str[0] + '0');
+				putchar(str[1] + '0');
+			}
+			
 		}
 	}
+	putchar('\n');
 
 	return (0);
 }
