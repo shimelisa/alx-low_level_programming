69f6e828774f8999ad92dfdc98270491ac6d41a4
shimelisa <shimyk97@gmail.com>
2022-10-18 16:35:36 -0700

    h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,19 @@
 #include "main.h"
 /**
-* main - funciton prints text
+* main - program start
 * Return: 0
 */
 int main (void)
 {
-	char somebdy[10] = "_putchar";
-	int i;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
-	for (i = 0; i < 8; i++)
-		_putchar([somebdy[i]);
-
-	_putchar(10);
+	_putchar('\n');
 	return(0);
 }
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main"
+#include <unisted.h>
+/**
+* main - I don't know
+*
+* Return: Always 0
+*_putchar - writes char to stdout
+*/
+int _putchar(char c)
+{
+	return (write (1, &c, 1));
+}
