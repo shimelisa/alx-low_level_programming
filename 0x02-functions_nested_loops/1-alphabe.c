a4c5313b786bac415136be2b54b1c779064a15ad
shimelisa <shimyk97@gmail.com>
2022-10-18 16:54:17 -0700

    Print small letter alphabets

---
+++ 0x02-functions_nested_loops/1-alphabe.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - prgram start
+* Return: 0
+*/
+int main (void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphaabet++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
