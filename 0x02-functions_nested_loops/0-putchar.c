4dec55e162af5716a51470c68ca757ad838071a8
shimelisa <shimyk97@gmail.com>
2022-10-18 15:14:04 -0700

    hhhhhh

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <main.h.>
+/**
+* Function to write _putchar
+*/
+int main (void)
+{
+	char somebdy[] = "_putchar";
+
+	for (i = 0; i < len(somebbdy[]); i++)
+		_putchar([somebdy[i]);
+
+	return(0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This repo is for functions and nested loops
