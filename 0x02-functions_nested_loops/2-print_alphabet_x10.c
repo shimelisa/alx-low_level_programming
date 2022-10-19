45dd01bc2820949685d26cd6456c404c9f589985
shimelisa <shimyk97@gmail.com>
2022-10-19 00:57:49 -0700

    print lowercase alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print small alphabets 10 times
+* Return: void
+*/
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int i = 0;
+
+	while (i < 10)
+	{
+		for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		_putchar('\n')
+		i++:
+	}
+}
