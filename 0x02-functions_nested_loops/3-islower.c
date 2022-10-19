10944c5f702c6e0aae1eb7086183c97b97e424db
shimelisa <shimyk97@gmail.com>
2022-10-19 01:43:49 -0700

    check if it is lower or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _islower - Entry point
+* Description: checks a charcter and print 1if lower & o if not
+* @c: the integer value it receives
+* Return: 1 if true. 0 if false
+*/
+int _islower(int c)
+{
+	if (c < 97)
+		Return (0);
+	else if (c < 123)
+		Return (1);
+	else
+		Return (0);
+}
