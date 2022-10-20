8d365678110e84839d25337fb4eb22d54b77a548
shimelisa <shimyk97@gmail.com>
2022-10-20 10:06:40 -0700

    correct largest number checker

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* largest_numbr - returns the largest of 3 numbers
+* @a: firs integer
+* @b: second integer
+* @c: third integer
+* Return: largest number
+*/
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && a >= C)
+		{
+			largest = a;
+		}
+	else if (b >= a && b >= C)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (large);
+}
