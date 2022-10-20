115ac0e978c71e7bb5f5f91ba79c91f116f6bfac
shimelisa <shimyk97@gmail.com>
2022-10-19 19:19:46 -0700

    naturla number <1024 that are divisible by 3 or 5

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n >=0 && n < 16)
+	if (n >= 0 && n < 16)
 	{
 		for (i = 0; i <= n; i++)
 		{
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - print natual number below 1024 if multiple of 3 or 5
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = 1; a < 1024; a++)
+	{
+		if (((i % 3) == 0) || ((i % 5) == 0))
+			j += i;
+	}
+	printf("%d\n", j);
+	return (0);
+}
