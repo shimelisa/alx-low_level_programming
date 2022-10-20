f6f639fc9bcf77f8a979eae06772828da0ad6c00
shimelisa <shimyk97@gmail.com>
2022-10-19 18:00:53 -0700

    print number b/n 98 & given number

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,10 +8,10 @@
 void print_to_98(int n)
 {
 	if (n < 98)
-		for ( ;n < 98; n++)
+		for ( ; n < 98; n++)
 			printf("%d, ", n);
 	else if (n > 99)
-		for ( ;n > 98; n--)
+		for ( ; n > 98; n--)
 			printf("%d, ", n);
 	printf("%d\n", n);
 }
