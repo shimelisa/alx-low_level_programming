b719aae2d03318d7c14af9394578ef0f2125c12e
shimelisa <shimyk97@gmail.com>
2022-10-24 17:42:02 -0700

    Convert string to integer

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -8,12 +8,31 @@
 */
 int main(void)
 {
-	char s1[98];
-	char *ptr;
+	int nb;
 
-	ptr = _strcpy(s1, "First, sove the problem. Then, write the code\n");
-	printf("%s", s1);
-	printf("%s", ptr);
+	nb = _atoi("98");
+	printf("%d\n", nb);
+
+	nb = _atoi("-402");
+	printf("%d\n", nb);
+
+	nb = _atoi("             ------++++++-----+++++--98");
+	printf("%d\n", nb);
+
+	nb = _atoi("214748364");
+	printf("%d\n", nb);
+
+	nb = _atoi("0");
+	printf("%d\n", nb);
+
+	nb = _atoi("Suite 402");
+	printf("%d\n", nb);
+
+	nb = _atoi("   +   +   -   - 98 Battery Street; San Francissco CA 94111 - USA  ");
+	printf("%d\n", nb);
+
+	nb = _atoi("---+++ -++ Sui - te - 402 #cisfun :)");
+	printf("%d\n",nb);
 
 	return (0);
 }
