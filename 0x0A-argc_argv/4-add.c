869cc5461dc734116d6053de9c30b46a108043a7
shimelisa <shimyk97@gmail.com>
2022-11-04 18:06:07 -0700

    program to add tow positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-/**
-* check_char - check if given char is # or not
-* @c: pointer to char to be checked
-*
-* Return: -1 if it is character else actual numbe
-*/
-
-int check_char(char *c)
-{
-	int i = 0, j = 0, k = strlen(c);
-
-	while (i < k)
-	{
-		if (c[i] < '0' || c[i] > '9')
-			return (-1);
-		else
-			j = j * 10 + (c[i] - '0');
-		i++;
-	}
-
-	return (j);
-}
 
 /**
 * main - print programs name
@@ -34,18 +10,20 @@ int check_char(char *c)
 */
 int main(int argc, char *argv[])
 {
-	int l, m, sum = 0;
+	int i, j, sum = 0;
 
-	for (l = 1; l < argc; l++)
+	for (i = 1; i < argc; i++)
 	{
-		m = check_char(argv[l]);
-		if (m == -1)
+		for (j = 0; argv[i][j]; j++)
 		{
-			printf("Error\n");
-			return(1);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 
-		sum += m;
+		sum += atoi(argv[i]);
 	}
 
 	printf("%d\n", sum);
