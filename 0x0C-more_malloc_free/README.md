e22f10acc7fc252bb748fa1413299bd78320cf3a
shimelisa <shimyk97@gmail.com>
2022-11-10 09:14:56 -0800

    function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,5 @@
+# C-more malloc free calloc exit
+
+* more ‛malloc‛ & ‛free‛
+* additionally ‛calloc‛ & ‛exit‛ functions
+* excercise on the above subjects
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* main - header file for prototypes
+* Description: contains prototype of all function used
+*/
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+
+#endif
