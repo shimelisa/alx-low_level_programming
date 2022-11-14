6e5d7ce1e1f3f9246be560faf819664f95224ad7
shimelisa <shimyk97@gmail.com>
2022-11-14 10:15:19 -0800

    function-like macro ABS(x) that computes abolute value

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * ABS(x) - function like macro that define absolute value of x
+ * @x: integer to be absoluted
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : x)
+
+#endif
+
