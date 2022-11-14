66ca02a4ddbe336f5ea2defc425461c3edcaae9e
shimelisa <shimyk97@gmail.com>
2022-11-14 10:28:10 -0800

    function-like macr SUM(x, y) that compute summation

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * SUM - compute summation of two numbers
+ * @x: first element
+ * @y: send element
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif /* SUM_H */
