f96c91e3ac9fcceb92c32283cb786bb45ca363c1
shimelisa <shimyk97@gmail.com>
2022-11-28 13:24:50 -0800

    delete node at specific index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Inserts a new node to a linked list
+ * @head: A pointer to the address of head of the linked list
+ * @idx: index of linked list where the new node is started (0 is start)
+ * @n: The integer for the new node to contain.
+ *
+ * Return: NULL pr address of the new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *copy = *head;
+	unsigned int node;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = copy;
+		*head = new;
+		return (new);
+	}
+
+	for (node = 0; node < (idx - 1); node++)
+	{
+		if (copy == NULL || copy->next == NULL)
+			return (NULL);
+
+		copy = copy->next;
+	}
+
+	new->next = copy->next;
+	copy->next = new;
+
+	return (new);
+}
