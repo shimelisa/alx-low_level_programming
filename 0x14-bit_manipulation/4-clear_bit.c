30ea92cf3d33f6aaad8dbbf8fb836ccb68a8b9fe
shimelisa <shimyk97@gmail.com>
2022-12-01 09:33:21 -0800

    clear i'th bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * set_bit - sets bit at index to 1
  * @n: pointer to number
@@ -6,7 +7,7 @@
  *
  * Return: 1 on success, -1 on failure
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= 8 * sizeof(*n))
 		return (-1);
